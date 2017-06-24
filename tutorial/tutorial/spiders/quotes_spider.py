5303177c0c6e1f56d5212f419801bdc862333888
mansiigoel <mansi.mie@gmail.com>
2017-06-24 11:45:01 +0530

    crawler to extract the data from a website like quotes.toscrape.com...

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+#if we dont know how many pages are there in the website
+
+         #next_page = response.css('li.next a::attr(href)').extract_first()
+        #if next_page is not None:
+            #yield response.follow(next_page, callback=self.parse)
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
